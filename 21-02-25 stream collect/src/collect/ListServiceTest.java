9bc06fa39473c6f64ac3cdef11e4ffee7d61214a
Nadja2310 <maksakova.nadja@gmail.com>
2021-02-26 00:11:58 +0100

    collect stream1

--- 21-02-25 stream collect/src/collect/ListServiceTest.java
+++ 21-02-25 stream collect/src/collect/ListServiceTest.java
@@ -8,11 +8,11 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListServiceTest {
-    ListService listService =new ListService();
+    ListService listService = new ListService();
 
     @org.junit.jupiter.api.Test
     void sumOfSquareOfNumbers() {
-        assertEquals(21, listService.sumOfSquareOfNumbers(Arrays.asList(2,4,1)));
+        assertEquals(21, listService.sumOfSquareOfNumbers(Arrays.asList(2, 4, 1)));
     }
 
     @org.junit.jupiter.api.Test
@@ -22,21 +22,21 @@ class ListServiceTest {
 
     @org.junit.jupiter.api.Test
     void verify() {
-        List<String> list=Arrays.asList("ehe","aha","ufo","sort");
-        Map<Boolean,List<String>> res= listService.verifyPalindrome(list);
-        List<String> listTrue=Arrays.asList("ehe","aha");
-        List<String> listFalse=Arrays.asList("ufo","sort");
-        assertEquals(listTrue,res.get(true));
-        assertEquals(listFalse,res.get(false));
+        List<String> list = Arrays.asList("ehe", "aha", "ufo", "sort");
+        Map<Boolean, List<String>> res = listService.verifyPalindrome(list);
+        List<String> listTrue = Arrays.asList("ehe", "aha");
+        List<String> listFalse = Arrays.asList("ufo", "sort");
+        assertEquals(listTrue, res.get(true));
+        assertEquals(listFalse, res.get(false));
     }
 
     @org.junit.jupiter.api.Test
     void verify_onlyTrue() {
-        List<String> list=Arrays.asList("Город у рва мавру дорог","У дуба буду");
-        Map<Boolean,List<String>> res= listService.verifyPalindrome(list);
-        List<String> listTrue=Arrays.asList("Город у рва мавру дорог","У дуба буду");
-        List<String> listFalse= Collections.emptyList();
-        assertEquals(listTrue,res.get(true));
-        assertEquals(listFalse,res.get(false));
+        List<String> list = Arrays.asList("Город у рва мавру дорог", "У дуба буду");
+        Map<Boolean, List<String>> res = listService.verifyPalindrome(list);
+        List<String> listTrue = Arrays.asList("Город у рва мавру дорог", "У дуба буду");
+        List<String> listFalse = Collections.emptyList();
+        assertEquals(listTrue, res.get(true));
+        assertEquals(listFalse, res.get(false));
     }
 }
