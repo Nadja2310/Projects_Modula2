23defa3e79b9558ee36c85820f5868903f2226e3
Nadja2310 <maksakova.nadja@gmail.com>
2021-02-26 00:01:30 +0100

    collect stream

---
+++ 21-02-25 stream collect/src/collect/ListService.java
@@ -0,0 +1,26 @@
+package collect;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListService {
+    public int sumOfSquareOfNumbers(List<Integer> list){
+        return list.stream()
+                .collect(Collectors.summingInt(number->number*number));
+    }
+
+    public Map<Boolean,List<String>> verifyPalindrome(List<String> list){
+
+        return  list.stream()
+                .collect(Collectors.partitioningBy(this::isPalindrome));
+
+    }
+
+    private boolean isPalindrome(String str){
+        String inputStr=str.replaceAll(" ","").toLowerCase();
+        StringBuilder stringBuilder=new StringBuilder(inputStr);
+        StringBuilder stringBuilderRevers=stringBuilder.reverse();
+        return stringBuilderRevers.toString().equals(inputStr);
+    }
+}
---
+++ 21-02-25 stream collect/src/collect/ListServiceTest.java
@@ -0,0 +1,42 @@
+package collect;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListServiceTest {
+    ListService listService =new ListService();
+
+    @org.junit.jupiter.api.Test
+    void sumOfSquareOfNumbers() {
+        assertEquals(21, listService.sumOfSquareOfNumbers(Arrays.asList(2,4,1)));
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumOfSquareOfNumbers_sum0() {
+        assertEquals(0, listService.sumOfSquareOfNumbers(Arrays.asList(0)));
+    }
+
+    @org.junit.jupiter.api.Test
+    void verify() {
+        List<String> list=Arrays.asList("ehe","aha","ufo","sort");
+        Map<Boolean,List<String>> res= listService.verifyPalindrome(list);
+        List<String> listTrue=Arrays.asList("ehe","aha");
+        List<String> listFalse=Arrays.asList("ufo","sort");
+        assertEquals(listTrue,res.get(true));
+        assertEquals(listFalse,res.get(false));
+    }
+
+    @org.junit.jupiter.api.Test
+    void verify_onlyTrue() {
+        List<String> list=Arrays.asList("Город у рва мавру дорог","У дуба буду");
+        Map<Boolean,List<String>> res= listService.verifyPalindrome(list);
+        List<String> listTrue=Arrays.asList("Город у рва мавру дорог","У дуба буду");
+        List<String> listFalse= Collections.emptyList();
+        assertEquals(listTrue,res.get(true));
+        assertEquals(listFalse,res.get(false));
+    }
+}
